8e1e5c8ce262ef1c131dcb7c3ae8040aeed1d4e5
Jontomber <Jonathan.berglund@edu.newton.se>
2024-09-02 10:12:19 +0200

    New

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("Program begin");
+
+        
+            int sum = 0;
+            for (int i = 0; i < 4; i++)
+            {
+
+                sum += i;
+                Console.WriteLine($"i = {i}\nsum = {sum}");
+
+                if (sum == 3)
+                {
+                    Console.WriteLine("We have the magical sum of 3");
+
+                }
+            }
+            
+            Console.WriteLine("Program end");
+
+
+        }
+    }  
+
+}
